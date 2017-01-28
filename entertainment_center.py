cda99fc6d4b46ae11b0513872664aeda0e9d3d3c
Utkarsh Trehan <utkarshtrehan@gmail.com>
2017-01-28 22:10:06 +0530

    Add main method file

---
+++ entertainment_center.py
@@ -0,0 +1,25 @@
+import media
+from popular import get_movie_list
+import fresh_tomatoes
+
+movies_list = get_movie_list()
+print(movies_list)
+
+
+
+
+
+
+# toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life",
+#                         "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+#                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+#
+# avatar = media.Movie("Avatar",
+#                      "A marine on an alien planet",
+#                      "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+#                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+#
+# bvs = media.Movie("Batman vs Superman",
+#                                  " American superhero film featuring the DC Comics characters Batman and Superman",
+#                                  "https://upload.wikimedia.org/wikipedia/en/2/20/Batman_v_Superman_poster.jpg",
+#                                  "https://www.youtube.com/watch?v=eX_iASz1Si8")
