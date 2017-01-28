86d9cc1e22e1a7c6f5adbaaf361b376faa93e029
Utkarsh Trehan <utkarshtrehan@gmail.com>
2017-01-29 00:00:18 +0530

    Bug Fixes
    Bug Fixes

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,24 +2,13 @@ import media
 from popular import get_movie_list
 import fresh_tomatoes
 
+list_of_movies = list()
 movies_list = get_movie_list()
 print(movies_list)
+for movie in movies_list:
+    data = movies_list[movie]
+    list_of_movies.append(media.Movie(data['title'],data['overview'],
+                                      data['poster_path'],
+                                      "https: // www.youtube.com / watch?v = gkV_SowjNFk"))
+fresh_tomatoes.open_movies_page(list_of_movies)
 
-
-
-
-
-
-# toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life",
-#                         "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-#                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
-#
-# avatar = media.Movie("Avatar",
-#                      "A marine on an alien planet",
-#                      "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
-#                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
-#
-# bvs = media.Movie("Batman vs Superman",
-#                                  " American superhero film featuring the DC Comics characters Batman and Superman",
-#                                  "https://upload.wikimedia.org/wikipedia/en/2/20/Batman_v_Superman_poster.jpg",
-#                                  "https://www.youtube.com/watch?v=eX_iASz1Si8")
